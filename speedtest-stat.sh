a11caccce143551316de0f8e322e7c678d24463c
Joakim Antman <antmanj@gmail.com>
2015-05-31 22:41:03 +0300

    fixed the if statements to work on the raspi

--- speedtest-stat.sh
+++ speedtest-stat.sh
@@ -5,7 +5,7 @@ THINGSPEAK_APIKEY=$1
 SPEEDTEST_SERVER="4549" #Helsinki, Elisa Oyj
 RESULT_RAW=$(speedtest-cli --server $SPEEDTEST_SERVER --simple)
 #DBGRESULT_RAW="ping: 20.031 Z download: 9.07 X upload: 0.80 Y"
-rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+rc=$?; if [ $rc != 0 ]; then exit $rc; fi
 
 #AWK Version for the case when newlines would be preserved
 #RES_PING=$(echo $RESULT_RAW | awk 'BEGIN {FS=" ";} { if($1 == "Ping:"){ print $2; } }')
@@ -16,7 +16,7 @@ RES_UPLOAD=$(echo $RESULT_RAW | cut -d' ' -f8)
 
 echo "ping: $RES_PING download: $RES_DOWNLOAD upload: $RES_UPLOAD"
 
-if [[ "$THINGSPEAK_APIKEY" != "" ]]
+if [ "$THINGSPEAK_APIKEY" != "" ]
 then
     echo "Pushing results to thinkspeak.com"
     curl -k --data "api_key=$THINGSPEAK_APIKEY&field1=$RES_PING&field2=$RES_DOWNLOAD&field3=$RES_UPLOAD" https://api.thingspeak.com/update
